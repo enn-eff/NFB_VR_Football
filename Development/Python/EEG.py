49485c6c8f3a2133c82d84a631e143367cc124ab
Nauman Fakhar <s8nafakh@stud.uni-saarland.de>
2020-09-30 13:58:22 +0200

    pushing PC version to GIT(removing streamVRInput and adding Python updated classes)

--- Development/Python/EEG.py
+++ Development/Python/EEG.py
@@ -1,52 +1,90 @@
-
-#from Python.src.Unicorn_Recorder.unicorn_recorder import Unicorn_recorder
-# Try activating different plot classes here to see the functionality
-from Python.src.Plotter.Plots.SwitchPlot import SwitchPlot as Plotclass
-#from Plotter.Plots.SwitchPlot import SwitchPlot as Plotclass
-
-from Python.UnicornRecorder import Unicorn_recorder
-
 from Python.EEGAcquisition import UnicornRecorder
-# Note that since we are working with multiple processes this if clause is necessary.
+from matplotlib.axis import Axis
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.signal import filtfilt
+import pyqtgraph as pg
+import scipy
+from collections import deque
+from pyqtgraph.Qt import QtGui, QtCore
 
 import time
 
 class EEG:
     def __init__(self):
-        rec1 = UnicornRecorder()
-        rec1.connect()
-        rec1.start_recording(test_signal_mode=False)
+        """
+        Plotting region
+        """
+        self.dat = deque()
+        self.maxLen = 50  # max number of data points to show on graph
+        self.app = QtGui.QApplication([])
+        self.win = pg.GraphicsWindow()
 
-        #a = rec1.get_data()[1, :]
-        #rec1.custom_Plot()
-
-        #rec1.toggle_plot(1, Plotclass)
-        rec1.refresh()
-        #a = rec1.get_data()[1, :]
-
-        #plt.plot(10, a)
-        #plt.show()
-        ##rec.get_Configuration()
-        ##int(rec.get_Configuration())
-        #rec.start_recording(test_signal_mode=False)
-
-        #rec.toggle_plot(1, Plotclass)
-
-        #rec.refresh()
-        #a = rec.get_data()[1,:]
-
-        #b = rec.get_data()[3,:]
-
-
-        #from scipy import signal
-        #psd = signal.periodogram(a, 250)
-        #abc = signal.periodogram(b, 250)
+        """ 
+            #End Region
+        """
+        self.rec1 = UnicornRecorder()
+        self.rec1.connect()
+        self.rec1.start_recording(test_signal_mode=False)
         #time.sleep(1)
+        self.fs = 250.0
+        self.acquiring_data(self)
+
+    def acquiring_data(self):
+        num = 0
+        plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        line, = ax.plot([], [])
+        T = 60
+        plt.xlim([0, T])
+        # Horizontal window range (should be fixed)
+        plt.ylim([-500, 500])
+        # Vertical window range (should be fixed)
+        #line1, = ax.plot([], [])
+        #plt.plot([], [])
+        #plt.draw()
+        #fig = plt.gcf()
+        #fig.show()
+        #fig.canvas.draw
+        while True:
+            self.rec1.refresh()
+            data = self.rec1.get_data()
+            a = data[1, :]
+            b = data[3, :]
+            #print(data)
+
+            if a.size > 15:
+                filteredSignal = self.filt(self, a)
+                # 15000 = self.fs (250) * 1 min (60)
+                tim = np.arange(15000 - a.size, 15000)/self.fs
+                #tim = np.arange(0, a.size) / self.fs
+                # ^ Plot the filtered "a" array against the last samples
+                # which are "a.size" in number. In this way the plot curve
+                # will move from right to left. If we use instead
+                # tim = np.arange(0, a.size)/self.fs, we will not get our
+                # desired plot.
+                line.set_ydata(filteredSignal)
+                line.set_xdata(tim)
+                fig.canvas.draw()
+
+                plt.pause(0.01)
+
+            if num == 1000:
+                break
+            num += 1
 
 
-        return b"Nauman"
+    def filt(self, sigA):
+        lowcut, highcut = 0.5, 50.0
+        nyq = 0.5 * self.fs
+        # below lines are converting analog into digital
+        low = lowcut/nyq
+        high = highcut/nyq
+        order = 2
+        b, a = scipy.signal.butter(order, [low, high], 'bandpass', analog=False)
+        filtSignal = scipy.signal.filtfilt(b, a, sigA)
 
-
-    #def filter(self, x):
+        return filtSignal
 
--- Development/Python/EEGAcquisition.py
+++ Development/Python/EEGAcquisition.py
@@ -11,6 +11,11 @@ import numpy
 import logging
 import time
 import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.signal import filtfilt
+import scipy
+import time
+
 
 class UnicornRecorder:
     # The space needed to store a single value from any channel
@@ -110,7 +115,7 @@ class UnicornRecorder:
 
             with self.__unpacked_data_lock:
                 self.__unpacked_data.append(buffer)
-            print(self.__unpacked_data[0])
+            #print(self.__unpacked_data[0])
         # the following statement will only run when the loop is finished
         print(self.__unpacked_data[20])
 
@@ -124,7 +129,7 @@ class UnicornRecorder:
         :return:
         """
         with self.__data_lock:
-            data = self.__data[:, cutoff:].copy()
+            data = self.__data[:, -250*60:].copy()
         return data
 
     def refresh(self):
@@ -161,69 +166,64 @@ class UnicornRecorder:
                 self.__data = numpy.append(self.__data, numpy.matrix(samples).transpose(), axis=1).getA()
 
 
-    #def custom_Plot(self):
+    #def toggle_plot(self, ref_interval, plot_class, info_queue=None, event_queue=None):
+    #    """
+    #    Enables an automatic plot for the incoming EEG data
+    #    :param ref_interval: the interval in which the plot should be updated.
+    #    :return:
+    #    """
+    #    if self.__plotting_thread is None:
+    #        self.__plotting_thread = threading.Thread(name="PlottingThread", target=self.__plotting_loop,
+    #                                                  args=(ref_interval, plot_class, info_queue, event_queue))
+    #        self.__plotting_thread.start()
+    #    else:
+    #        self.__plotting_thread.is_running = False
+    #        self.__recording_thread.join()
+    #        self.__plotting_thread = None
 #
-    #    plt.plot(a, b)
-    #    plt.show()
-
-    def toggle_plot(self, ref_interval, plot_class, info_queue=None, event_queue=None):
-        """
-        Enables an automatic plot for the incoming EEG data
-        :param ref_interval: the interval in which the plot should be updated.
-        :return:
-        """
-        if self.__plotting_thread is None:
-            self.__plotting_thread = threading.Thread(name="PlottingThread", target=self.__plotting_loop,
-                                                      args=(ref_interval, plot_class, info_queue, event_queue))
-            self.__plotting_thread.start()
-        else:
-            self.__plotting_thread.is_running = False
-            self.__recording_thread.join()
-            self.__plotting_thread = None
-
-    def __plotting_loop(self, ref_interval, plot_class, info_queue=None, event_queue=None):
-        """
-        Starts the Plotter in a separate process and continuously feeds it newly acquired data.
-        :param ref_interval:
-        :param plot_class:
-        :return:    #TODO close plots
-        """
-        plot_process = Process(name="PlotProcess", target=UnicornRecorder.plot_wrapper,
-                               args=(self.__plot_queue, plot_class, self.__SFREQ, info_queue, event_queue))
-        plot_process.start()
-
-        with self.__data_lock:
-            observer_index = self.__data.shape[1]
-
-        while plot_process.is_alive():
-            before = time.time()
-            self.refresh()
-            data = self.get_new_data(observer_index)
-            self.__plot_queue.put(data)
-
-            observer_index += data.shape[1]
-            t = ref_interval - (time.time() - before)
-            time.sleep(0 if t <= 0 else t)
-
-    @staticmethod
-    def plot_wrapper(queue, plot_class, sfreq, info_queue=None, event_queue=None):
-        """
-        A simple wrapping function for starting a plot in another Process
-        :param queue: a multiprocessing queue EXCLUSIVELY used for transferring EEG data
-        :param sfreq: the sampling frequency
-        :param plot_class: The class defining a Plot
-        :return:
-        """
-        plot = plot_class(queue, sfreq, info_queue, event_queue)
-        plot.start()
-
-    def get_new_data(self, index=None):
-        """
-        Returns the new data acquired since the last call to refresh
-        :return:
-        """
-        with self.__data_lock:
-            if index is None:
-                return self.__data[:, self.__new_data_index:].copy()
-            else:
-                return self.__data[:, index:].copy()
+    #def __plotting_loop(self, ref_interval, plot_class, info_queue=None, event_queue=None):
+    #    """
+    #    Starts the Plotter in a separate process and continuously feeds it newly acquired data.
+    #    :param ref_interval:
+    #    :param plot_class:
+    #    :return:    #TODO close plots
+    #    """
+    #    plot_process = Process(name="PlotProcess", target=UnicornRecorder.plot_wrapper,
+    #                           args=(self.__plot_queue, plot_class, self.__SFREQ, info_queue, event_queue))
+    #    plot_process.start()
+#
+    #    with self.__data_lock:
+    #        observer_index = self.__data.shape[1]
+#
+    #    while plot_process.is_alive():
+    #        before = time.time()
+    #        self.refresh()
+    #        data = self.get_new_data(observer_index)
+    #        self.__plot_queue.put(data)
+#
+    #        observer_index += data.shape[1]
+    #        t = ref_interval - (time.time() - before)
+    #        time.sleep(0 if t <= 0 else t)
+#
+    #@staticmethod
+    #def plot_wrapper(queue, plot_class, sfreq, info_queue=None, event_queue=None):
+    #    """
+    #    A simple wrapping function for starting a plot in another Process
+    #    :param queue: a multiprocessing queue EXCLUSIVELY used for transferring EEG data
+    #    :param sfreq: the sampling frequency
+    #    :param plot_class: The class defining a Plot
+    #    :return:
+    #    """
+    #    plot = plot_class(queue, sfreq, info_queue, event_queue)
+    #    plot.start()
+#
+    #def get_new_data(self, index=None):
+    #    """
+    #    Returns the new data acquired since the last call to refresh
+    #    :return:
+    #    """
+    #    with self.__data_lock:
+    #        if index is None:
+    #            return self.__data[:, self.__new_data_index:].copy()
+    #        else:
+    #            return self.__data[:, index:].copy()
